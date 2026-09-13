e1d8ca3341a467e08c55cbdcf516ab701c04b832
safarislava <safarislava@gmail.com>
2026-09-13 16:07:31 +0300

    feat: add run config

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from infrastructure.http import auth_router, health_router, user_router
@@ -7,3 +8,6 @@ app = FastAPI(title="Our site")
 app.include_router(health_router, prefix="/api")
 app.include_router(user_router, prefix="/api")
 app.include_router(auth_router, prefix="/api")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

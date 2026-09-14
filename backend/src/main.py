c10f0d3cda14a1a3be9a5b3611e5a87b0f01de32
safarislava <safarislava@gmail.com>
2026-09-14 12:28:36 +0300

    feat: set up roles rules

--- backend/src/main.py
+++ backend/src/main.py
@@ -14,6 +14,7 @@ from infrastructure.http.middleware.authorization_middleware import (
     RouteAuthRule,
     authorization_middleware,
 )
+from infrastructure.http.order.controller import router as order_router
 from infrastructure.http.product.controller import router as product_router
 from infrastructure.http.recovery.controller import router as recovery_router
 from infrastructure.http.teacher.controller import router as teacher_router
@@ -21,17 +22,26 @@ from infrastructure.http.user.controller import router as user_router
 
 auth_rules: list[RouteAuthRule] = [
     RouteAuthRule.create(
+        path="/api/orders/{order_id}/approve",
+        methods={"POST"},
+        allowed_roles={UserRole.ADMIN},
+    ),
+    RouteAuthRule.create(
         path="/api/products",
         methods={"POST", "PATCH", "DELETE"},
-        allowed_roles={UserRole.TEACHER, UserRole.ADMIN},
+        allowed_roles={UserRole.ADMIN},
     ),
     RouteAuthRule.create(
         path="/api/favourites",
         allowed_roles=set(UserRole),
     ),
     RouteAuthRule.create(
+        path="/api/orders",
+        allowed_roles=set(UserRole),
+    ),
+    RouteAuthRule.create(
         path="/api/teachers",
-        allowed_roles={UserRole.TEACHER, UserRole.ADMIN},
+        allowed_roles={UserRole.TEACHER},
     ),
 ]
 
@@ -49,6 +59,7 @@ app.include_router(product_router, prefix="/api")
 app.include_router(favourites_router, prefix="/api")
 app.include_router(teacher_router, prefix="/api")
 app.include_router(recovery_router, prefix="/api")
+app.include_router(order_router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
